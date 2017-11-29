38dd7dec95cd037e066511fbd58b65c2c4ee7b77
melad <melad>
2017-11-29 13:53:36 +0200

    initial commit

---
+++ app/src/main/java/net/wemakesites/em/bandschallenge/features/search/SearchBandsByNameActivity.java
@@ -0,0 +1,15 @@
+package net.wemakesites.em.bandschallenge.features.search;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.wemakesites.em.bandschallenge.R;
+
+public class SearchBandsByNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_band_by_name);
+    }
+}
